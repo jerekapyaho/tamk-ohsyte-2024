8d538a27174192cd01777103991fc018ea52d06f
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2024-02-15 11:51:50 +0200

    Added C# event test program

---
+++ csharp/Properties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+Event e1 = new Event(new DateOnly(2023, 11, 14), ".NET 8 released", "microsoft");
+//Console.WriteLine(e1);
+
+// https://learn.microsoft.com/en-us/dotnet/standard/datetime/how-to-use-dateonly-timeonly
+
+List<Event> events = new ();
+events.Add(e1);
+
+Event e2 = new Event(new DateOnly(2022, 11, 8), ".NET 7 released", "microsoft");
+events.Add(e2);
+
+events.Add(new Event(new DateOnly(2021, 11, 8), ".NET 6 released", "microsoft"));
+
+foreach (Event e in events)
+{
+    Console.WriteLine(e);
+}
+
+events.Sort();  // impossible unless Event implements IComparable!
+Console.WriteLine("\nSorted events:");
+foreach (Event e in events)
+{
+    Console.WriteLine(e);
+}
