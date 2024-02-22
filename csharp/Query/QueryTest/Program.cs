a4b674ed6284944db473645dd6d10fd69e1d4bf3
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2024-02-22 13:07:43 +0200

    Added LINQ fluent syntax example

--- csharp/Query/QueryTest/Program.cs
+++ csharp/Query/QueryTest/Program.cs
@@ -8,6 +8,10 @@ if (!eventManager.ReadEvents())
     Environment.Exit(0);
 }
 
+//
+// Simple LINQ query
+//
+
 var ms_events =
     from e in eventManager.Events
     where e.Category.Primary.Equals("microsoft")
@@ -18,6 +22,10 @@ foreach (var e in ms_events)
 {
     Console.WriteLine(e);
 }
+
+//
+// LINQ query with filtering and ordering
+//
 
 var macos_events =
     from e in eventManager.Events
@@ -32,3 +40,21 @@ foreach (var e in macos_events)
     Console.WriteLine(e);
 }
 
+var month = 11;
+var day = 8;
+DateOnly d = new DateOnly(2024, month, day);
+
+//
+// LINQ-to-objects query using fluent syntax
+//
+
+var dayQuery = eventManager.Events
+    .Where(e => e.Date.Month == d.Month && e.Date.Day == d.Day)
+    .OrderBy(e => e.Date)
+    .Select(e => $"{e.Date.Year}: {e.Description}");
+
+Console.WriteLine($"\nEvents on YYYY-{month:D02}-{day:D02}");
+foreach (var e in dayQuery)
+{
+    Console.WriteLine(e);
+}
